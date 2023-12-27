998047c3ede37131dde5f4a1f6cef6a9f674f108
yubangxu85 <yubangxu85@126.com>
2023-12-27 12:44:16 +0800

    1、增加本地dns缓存，支持文件中配置过期时间

--- main.go
+++ main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/miekg/dns"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"strings"
-
-	"github.com/miekg/dns"
+	"sync"
+	"time"
 )
 
-type Config struct {
-	RecursiveDNS string `json:"recursive_dns"`
-	Whitelist    string `json:"whitelist"`
-	LogFile      string `json:"log_file"`
+type CachedResponse struct {
+	Msg      *dns.Msg
+	ExpireAt time.Time
 }
 
-var logger *log.Logger
+type Config struct {
+	RecursiveDNS    string        `json:"recursive_dns"`
+	Whitelist       string        `json:"whitelist"`
+	LogFile         string        `json:"log_file"`
+	CacheTimeoutStr string        `json:"cache_timeout"`
+	CacheTimeout    time.Duration // Add cache timeout configuration
+}
+
+var (
+	logger     *log.Logger
+	cache      map[string]CachedResponse
+	cacheMutex sync.RWMutex
+)
 
 func loadConfig(filename string) (Config, error) {
 	var config Config
@@ -35,7 +47,51 @@ func loadConfig(filename string) (Config, error) {
 		return config, err
 	}
 
+	// 转换字符串为 time.Duration
+	if config.CacheTimeoutStr != "" {
+		duration, err := time.ParseDuration(config.CacheTimeoutStr)
+		if err != nil {
+			return config, err
+		}
+		config.CacheTimeout = duration
+	}
 	return config, nil
+}
+
+// ... (Other functions remain unchanged)
+
+func lookupFromCache(queryKey string) (*dns.Msg, bool) {
+	cacheMutex.RLock()
+	defer cacheMutex.RUnlock()
+	cachedResponse, found := cache[queryKey]
+	if !found || time.Now().After(cachedResponse.ExpireAt) {
+		return nil, false
+	}
+	return cachedResponse.Msg.Copy(), true
+}
+
+func storeInCache(queryKey string, response *dns.Msg, timeout time.Duration) {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+	cache[queryKey] = CachedResponse{
+		Msg:      response.Copy(),
+		ExpireAt: time.Now().Add(timeout),
+	}
+}
+
+func isIPInRange(ip string, ipRanges []*net.IPNet) bool {
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		return false
+	}
+
+	for _, ipNet := range ipRanges {
+		if ipNet.Contains(parsedIP) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func loadIPRangesFromFile(filepath string) ([]*net.IPNet, error) {
@@ -61,21 +117,6 @@ func loadIPRangesFromFile(filepath string) ([]*net.IPNet, error) {
 	return ipRanges, nil
 }
 
-func isIPInRange(ip string, ipRanges []*net.IPNet) bool {
-	parsedIP := net.ParseIP(ip)
-	if parsedIP == nil {
-		return false
-	}
-
-	for _, ipNet := range ipRanges {
-		if ipNet.Contains(parsedIP) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg, config Config) {
 	m := new(dns.Msg)
 	m.SetReply(r)
@@ -89,6 +130,21 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg, config Config) {
 	for _, q := range r.Question {
 		name := strings.ToLower(q.Name)
 		if q.Qtype == dns.TypeA {
+			queryKey := fmt.Sprintf("%s-%d", name, q.Qtype)
+
+			if cachedResponse, found := lookupFromCache(queryKey); found {
+				logger.Println("Cache hit for:", name)
+
+				resp := new(dns.Msg)
+				resp.SetReply(r)
+				resp.Answer = cachedResponse.Answer // Copy answers from cached response
+
+				resp.Id = r.Id // Set the response ID to match the query ID
+
+				w.WriteMsg(resp)
+				return
+			}
+
 			ip, err := net.LookupIP(name)
 			if err == nil {
 				for _, addr := range ip {
@@ -100,11 +156,13 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg, config Config) {
 						}
 						m.Answer = append(m.Answer, rr)
 						logger.Println("localDNS:", m.String())
+						storeInCache(queryKey, m, config.CacheTimeout) // Cache the response
 						w.WriteMsg(m)
 						return
 					}
 				}
 			}
+
 			recursiveAddr := config.RecursiveDNS
 			c := new(dns.Client)
 			resp, _, err := c.Exchange(r, recursiveAddr+":53")
@@ -113,6 +171,7 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg, config Config) {
 				return
 			}
 			logger.Println("recursiveDNS:", resp.String())
+			storeInCache(queryKey, resp, config.CacheTimeout) // Cache the response
 			w.WriteMsg(resp)
 		}
 	}
@@ -132,6 +191,8 @@ func main() {
 		fmt.Println("Error loading config:", err)
 		os.Exit(1)
 	}
+
+	cache = make(map[string]CachedResponse) // Initialize the cache
 
 	setupLogger(config.LogFile)
 
